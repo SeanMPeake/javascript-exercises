8d259c4c24b495e2f61af97967ad2b1d1e2a4a1e
SeanMPeake <171083841+SeanMPeake@users.noreply.github.com>
2024-07-12 15:57:06 -0500

    Test using split, reverse, and join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
 const reverseString = function(str) {
-    return str.split('').reduce((acc, char) => char + acc, '');
+
+    
+    // let reverseStr = str.split('');
+    // reverseStr = reverseStr.reverse();
+    // reverseStr = reverseStr.join('');
+    // return reverseStr;
+    // return str.split('').reverse().join('');
+    
+    // let revStr = str.split('');
+    // revStr = revStr.reduce((acc, char) => char + acc, '');
+    // return revStr;
+    // return str.split('').reduce((acc, char) => char + acc, '');
 };
 
 // Do not edit below this line
