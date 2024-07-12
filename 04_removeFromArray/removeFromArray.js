7eee89e0e9200213045e89fa69da763ef942940b
SeanMPeake <171083841+SeanMPeake@users.noreply.github.com>
2024-07-12 16:29:34 -0500

    Add code to pass first 3 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num, num2 = null) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== num && arr[i] !== num2) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
