e289d50471b123c29a01c4fa2b3b2af577dc599c
SeanMPeake <171083841+SeanMPeake@users.noreply.github.com>
2024-07-15 09:53:13 -0500

    Add solution for test 1 - working

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let lowerBounds,
+        upperBounds;
+
+    if (num1 < num2) {
+        lowerBounds = num1;
+        upperBounds = num2;
+    } else if (num1 > num2) {
+        lowerBounds = num2;
+        upperBounds = num1;
+    } else {
+        return num1 + num2;
+    }
+
+    let differenceBetween =  upperBounds - lowerBounds;
+
+    let finalSum = 0;
+    for (i = lowerBounds; i <= upperBounds; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 
 };
 
