8d201c44a238158b33979109c5b37be06e9e4764
SeanMPeake <171083841+SeanMPeake@users.noreply.github.com>
2024-07-15 10:29:33 -0500

    Add conditional logic for century years
    Tests 4, 5, 6 pass.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
 const leapYears = function(year) {
     if (year % 4 !== 0) {
-            return false;
+        return false;
+    } else if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
     } else {
         return true;
     }
