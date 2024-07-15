8f662d28b5d49a77d3f94b51294122f99c326cec
SeanMPeake <171083841+SeanMPeake@users.noreply.github.com>
2024-07-15 10:23:44 -0500

    Add initial code, test 1 passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+            return true;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// year % 100 === 0 &&
+// year % 400 === 0
